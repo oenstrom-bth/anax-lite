1f609eee981fdf60bbe29cb5761268142c1262df
Olof Enström <olofenstrom@host-193-11-187-114.eduroam.bth.se>
2017-04-02 15:58:53 +0200

    Added active styling, report kmom01, viewport meta tag

--- REVISION.md
+++ REVISION.md
@@ -1,14 +1,19 @@
 Revision History
 =======================
 
-v1.1.0 (2017-03-31)
+1.2.0 (2017-04-02)
 -----------------------
-* Second version.
+* Active menu item style added.
+* Report text for kmom01 added.
+* Viewport meta tag added.
+
+1.1.0 (2017-03-31)
+-----------------------
 * Theme added. Basic styling and structure.
 * Me page, about page, report page and test page added.
 * Navbar added (part 1).
 * Other small improvements.
 
-v1.0.0 (2017-03-29)
+1.0.0 (2017-03-29)
 -----------------------
-* First version, base framework.
+* Base framework.
--- view/take1/report.php
+++ view/take1/report.php
@@ -8,28 +8,48 @@
                 <hr>
 
                 <h2 id="kmom01">Kmom01</h2>
-                <p>Redovisningstext för kmom01 här.</p>
+                <h3>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</h3>
+                <p>Tycker det gick bra. Det var lite annorlunda mot Python men känner ändå att jag fick ganska bra grepp om det. Det är lite mer saker att hålla reda på i PHP jämfört med objektorienterad Python.</p>
+                <p>Guess the number-uppgiften gick bra att göra och hade inga speciella problem med den. Det tog lite längre tid att göra den än vad det borde. Hade svårt att koncentrera mig när jag gjorde den, men det blev bra till slut. Att göra den med ett objekt i sessionen tycker jag absolut var det smidigaste och bästa sättet.</p>
+                <p>När det kommer till extrauppgifterna i “Guess the number”, valde jag att ge sidorna ett enkelt och stilrent utseende. Jag lade inte jättemycket tid på att styla men tycker ändå resultatet blev snyggt och bra. Jag valde däremot att inte integrera det i ramverket.</p>
 
+                <h3>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</h3>
+                <p>Tycker det ska bli riktigt kul att lära sig göra ramverk och få lite koll på hur det fungerar. Tillsammans med den andra kursen så tog uppgifterna ganska mycket tid vilket gjorde att jag inte hade tid att försöka ändra och förbättra ramverket själv. Men det kommer väl längre in i kursen.</p>
+                <p>Jag skapade en vy för byline, en för flash-bild och en för en testsida. Flash-bilden använder jag på varje sida men bylinen finns däremot inte på förstasidan. Jag använder också Cimage på sidan för att hantera bilderna.</p>
+                <p>Navbaren valde jag att dela upp i två filer, en config-fil och en vy-fil. Config-filen returnerar en array med alla routes som ska skapas. Vy-filen använder sedan en rekursiv funktion för att generera html-koden. Den har alltså stöd för ett godtyckligt antal undermenyer.</p>
+                <p>När det kommer till styling av sidan valde jag att använda LESS. LESS-filerna lade jag i mappen “theme” och där i finns även en make-fil för att kompilera LESS till CSS. Det fungerar alltså likt sättet vi använde oss av i Anax Flat. För att inte helt behöva bygga upp allt från grunden använder jag mig av Preboot. Detta ger mig bland annat ett enkelt gridsystem att utgå ifrån och lite annat smått och gott. Jag känner att jag får mycket större frihet med Preboot jämfört med Bootstrap, något jag gillar.</p>
+                <p>För tillfället ligger den mesta LESS-koden i samma fil. Jag planerar dock att dela upp det i flera olika moduler för att få bättre struktur. Navbaren har däremot en egen modul och jag har byggt upp den helt själv från grunden. Det finns lite saker att förbättra med den, men jag är väldigt nöjd med den trots att det inte är något jätteavancerat. Den har till exempel stöd för hur många undermenyer som helst.</p>
+
+                <h3>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</h3>
+                <p>Det var absolut inga problem. Vi har i tidigare kurser jobbat med SQLite vilket gjorde allt lättare. Jag har även tidigare använt både MySQL och MariaDB så det var inga nyheter precis.</p>
+                <p>I uppgiften jobbade jag mot BTHs server vilket gjorde att jag inte kunde skapa och ta bort databaser. Jag använde mig av engelska namn istället för svenska, då det känns bättre helt enkelt. Jag gjorde fram till övning 9 i uppgiften och hade inga problem alls. Kommer nog göra klart uppgiften i nästa kursmoment.</p>
+
+                <hr>
 
                 <h2 id="kmom02">Kmom02</h2>
                 <p>Redovisningstext för kmom02 här.</p>
 
+                <hr>
 
                 <h2 id="kmom03">Kmom03</h2>
                 <p>Redovisningstext för kmom03 här.</p>
 
+                <hr>
 
                 <h2 id="kmom04">Kmom04</h2>
                 <p>Redovisningstext för kmom04 här.</p>
 
+                <hr>
 
                 <h2 id="kmom05">Kmom05</h2>
                 <p>Redovisningstext för kmom05 här.</p>
 
+                <hr>
 
                 <h2 id="kmom06">Kmom06</h2>
                 <p>Redovisningstext för kmom06 här.</p>
 
+                <hr>
 
                 <h2 id="kmom10">Kmom10</h2>
                 <p>Redovisningstext för kmom10 här.</p>
